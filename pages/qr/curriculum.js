fc9191237cefad5e03588d03a41faa85122c51bd
Sean Spearman <smspearmandev@gmail.com>
2022-06-21 01:26:42 -0700

    Add a lil more to qr code page

--- pages/qr/curriculum.js
+++ pages/qr/curriculum.js
@@ -5,10 +5,16 @@ const CurriculumQR = () => {
   return (
     <>
       <h1>Origami Curriculum</h1>
+      Scan QR Code:
       <Image
         src={curriculumQR}
         alt="https://docs.google.com/document/d/1zYfA03vzZuM32DhBsXpkN6UoAOzaZXDDfSZi7IZHq4Y/edit?usp=sharing"
       />
+      Or click this{" "}
+      <a href="https://docs.google.com/document/d/1zYfA03vzZuM32DhBsXpkN6UoAOzaZXDDfSZi7IZHq4Y/edit?usp=sharing">
+        direct link
+      </a>
+      .
     </>
   );
 };
