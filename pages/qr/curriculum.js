1906dec6d80c137e24bf846842b831c3f378acf3
Sean Spearman <smspearmandev@gmail.com>
2022-06-21 01:40:19 -0700

    Fix layout of text

--- pages/qr/curriculum.js
+++ pages/qr/curriculum.js
@@ -5,16 +5,18 @@ const CurriculumQR = () => {
   return (
     <>
       <h1>Origami Curriculum</h1>
-      Scan QR Code:
+      <p>Scan QR Code:</p>
       <Image
         src={curriculumQR}
         alt="https://docs.google.com/document/d/1zYfA03vzZuM32DhBsXpkN6UoAOzaZXDDfSZi7IZHq4Y/edit?usp=sharing"
       />
-      Or click this{" "}
-      <a href="https://docs.google.com/document/d/1zYfA03vzZuM32DhBsXpkN6UoAOzaZXDDfSZi7IZHq4Y/edit?usp=sharing">
-        direct link
-      </a>
-      .
+      <p>
+        Or click this{" "}
+        <a href="https://docs.google.com/document/d/1zYfA03vzZuM32DhBsXpkN6UoAOzaZXDDfSZi7IZHq4Y/edit?usp=sharing">
+          direct link
+        </a>
+        .
+      </p>
     </>
   );
 };
