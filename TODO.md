82350a4bd6d67064aa05fbc66aee2ae742337cf4
Sean Spearman <smspearmandev@gmail.com>
2026-09-24 16:28:20 -0700

    Use schedule view for calendar instead of month

--- TODO.md
+++ TODO.md
@@ -1,8 +1,10 @@
 ## TODO
 
 - Add instagram feed
-- Make a nice header and body template.
+- Make responsive layout
+- Update to next@16 to fix critical security warning
 
 ## Done
 
+x Use schedule view for calendar
 x Unpublish origami-site on gh-pages (the actual version is on netlify) and remove gh-pages custom domain config
--- pages/index.js
+++ pages/index.js
@@ -18,8 +18,7 @@ export default function Home() {
         </div>
 
         <iframe
-          src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=b2NkczBtaDRmdHRydDc3aWQyZ2dnYTIxcDBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000&amp;showTitle=1"
-          style={{ border: "solid 1px #777" }}
+          src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;showPrint=0&amp;mode=AGENDA&amp;showTabs=0&amp;showCalendars=0&amp;showNav=0&amp;showTitle=0&amp;showDate=0&amp;showTz=0&amp;src=b2NkczBtaDRmdHRydDc3aWQyZ2dnYTIxcDBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23D50000"
           width="800"
           height="600"
           frameBorder="0"
